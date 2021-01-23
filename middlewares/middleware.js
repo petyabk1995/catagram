49ffc36e9b89fe86b537e1b9619a3802d4493e94
Milena Kyurkieva <Petya@Petyas-MacBook-Pro.local>
2021-01-23 23:11:37 +0200

    new

---
+++ middlewares/middleware.js
@@ -0,0 +1,13 @@
+function middleware(req, res, next) {
+    console.log('hello from middleware');
+
+    console.log(req.params);
+
+    if (req.params.catName) {
+        next();
+        return;
+    } 
+    res.status(403).send('You need to specify catId');
+}
+
+module.exports = middleware;
--- server.js
+++ server.js
@@ -1,8 +1,39 @@
 const express = require('express');
+const checkCatId = require('./middlewares/middleware');
+const logger = require('./middlewares/loggerMiddleware');
 const app = express();
 
+app.use('/static', express.static('public'));
+
+app.use(logger);
+
 app.get('/', (req, res) => {
-    console.log(res);
+    res.sendFile(__dirname + '/views/home.html');
 });
 
-app.listen(3000, () => console.log(`Server is running`));
+app.get('/download', (req, res) => {
+    res.download('./views/home.html');
+});
+
+app.post('/cats', (req, res) => {
+    console.log('creating cat');
+    res.status(201).send('Cat created');
+});
+
+app.get('/cats/:catName?', checkCatId, (req, res) => {
+    res.send(`You are looking at ${req.params.catName}`)
+});
+
+
+// app.get('/cats/:catName/', (req, res) => {
+//     console.log(req.params);
+//     if (/\d+/.test(req.params.catName)) {
+//         res.status(404).send('You need a name');
+//         return;
+//     }
+
+//     res.send(`This is ${req.params.catName}`);
+// });
+
+
+app.listen(5000, () => console.log(`Server is running`));
