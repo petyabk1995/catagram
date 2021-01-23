a5638fa84e9abc3cb74cbd56c2ed6091de56a4c4
Milena Kyurkieva <Petya@Petyas-MacBook-Pro.local>
2021-01-23 19:12:53 +0200

    Express added

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log('Peti')
+const express = require('express');
+const app = express;
+
+app.get('/', (req, res) => {
+    console.log('Hello from console');
+
+    res.send('Hello world from express');
+})
+app.listen (5000, () => console.log('Server is running on port 5000...'));
