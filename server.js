b2254f3c5e1b055face44b0a72c0cceea4234983
Milena Kyurkieva <Petya@Petyas-MacBook-Pro.local>
2021-01-23 19:14:32 +0200

    new

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express;
 
 app.get('/', (req, res) => {
-    console.log('Hello from console');
-
     res.send('Hello world from express');
 })
-app.listen (5000, () => console.log('Server is running on port 5000...'));
+app.listen (5000, () => console.log(`Server is running on port 5000...`));
